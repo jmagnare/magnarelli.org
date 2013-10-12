ae46520ce2ac833dfacd712709736b1c00eb006e
James Magnarelli <jmagnare@gmail.com>
2013-10-12 03:17:58 -0400

    got graph totally working. Just need to add more likes

--- app/scripts/likesgraph.js
+++ app/scripts/likesgraph.js
@@ -11,6 +11,96 @@ function flatten(root) {
     recurse(root);
     return nodes;
 }
+
+// Toggle children on click.
+function click(d) {
+    if (d3.event.defaultPrevented) return; // ignore drag
+    if (d.children) {
+        d._children = d.children;
+        d.children = null;
+    } else {
+        d.children = d._children;
+        d._children = null;
+    }
+    update();
+}
+
+function update() {
+    var nodes = flatten(root);
+    var links = d3.layout.tree().links(nodes);
+    force
+        .nodes(nodes)
+        .links(links)
+        .start();
+    link = link.data(links, function(d) {return d.target.id});
+    link.exit().remove();
+    link
+        .enter().append('line')
+        .attr('class', 'link')
+        .attr('stroke-width', function(d) {return Math.sqrt(d.value);});
+/*
+    var node = svg.selectAll('.node')
+        .data(nodes)
+        .enter().append('g')
+        .attr('class', 'node')
+        .on('click', click)
+        .call(force.drag);
+
+
+    node.append('circle')
+        .attr('class', 'node')
+        .attr('r', 5)
+        .style('fill', function(d) { return color(d.group)});
+
+    node.append('text')
+        .attr('dx', 12)
+        .attr('dy', '.35em')
+        .text(function(d) { return d.name });
+  */
+    // Update nodes.
+    node = node.data(nodes, function(d) { return d.id; });
+
+    node.exit().remove();
+
+    var nodeEnter = node.enter().append("g")
+        .attr("class", "node")
+        .on("click", click)
+        .call(force.drag);
+
+    nodeEnter.append("circle")
+        .attr('class', 'node')
+        .attr('r', 5)
+        .style('fill', function(d) { return color(d.group)});
+
+    nodeEnter.append("text")
+        .attr("dy", ".35em")
+        .text(function(d) { return d.name; });
+
+    // Draw the "you can click me!" circles
+    node.select("circle")
+        .style("stroke", "#000000")
+        .style("stroke-width", getStrokeWidth);
+
+}
+
+function getStrokeWidth(d) {
+    if (d._children) {
+        return 2;
+    }
+    else
+    {
+        return 0;
+    }
+}
+function tick() {
+    link.attr('x1', function(d) { return d.source.x; })
+        .attr('y1', function(d) { return d.source.y; })
+        .attr('x2', function(d) { return d.target.x; })
+        .attr('y2', function(d) { return d.target.y; });
+
+    node.attr('transform', function(d) { return 'translate(' + d.x + ',' + d.y + ')'; });
+
+}
   /*
 function build_links(node) {
     var links = [];
@@ -31,57 +121,20 @@ var width = 960,
 var svg = d3.select('#d3').append('svg')
     .attr('width', width)
     .attr('height', height);
+var link = svg.selectAll(".link"),
+    node = svg.selectAll(".node");
+
+
 
 var force = d3.layout.force()
-    .gravity(.05)
+    .gravity(.005)
     .distance(100)
     .charge(-100)
+    .on("tick",tick)
     .size([width, height]);
 var color = d3.scale.category20();
 
 d3.json('misc/likes.json', function(json, error) {
-    var nodes = flatten(json);
-    var links = d3.layout.tree().links(nodes);
-    force
-        .nodes(nodes)
-        .links(links)
-        .start();
-
-    var link = svg.selectAll('.link')
-        .data(links)
-        .enter().append('line')
-        .attr('class', 'link')
-        .attr('stroke-width', function(d) {return Math.sqrt(d.value);});
-
-    var node = svg.selectAll('.node')
-        .data(nodes)
-        .enter().append('g')
-        .attr('class', 'node')
-        .call(force.drag);
-    /*
-     node.append('image')
-     .attr('xlink:href', 'https://github.com/favicon.ico')
-     .attr('x', -8)
-     .attr('y', -8)
-     .attr('width', 16)
-     .attr('height', 16);
-     */
-    node.append('circle')
-        .attr('class', 'node')
-        .attr('r', 5)
-        .style('fill', function(d) { return color(d.group)});
-
-    node.append('text')
-        .attr('dx', 12)
-        .attr('dy', '.35em')
-        .text(function(d) { return d.name });
-
-    force.on('tick', function() {
-        link.attr('x1', function(d) { return d.source.x; })
-            .attr('y1', function(d) { return d.source.y; })
-            .attr('x2', function(d) { return d.target.x; })
-            .attr('y2', function(d) { return d.target.y; });
-
-        node.attr('transform', function(d) { return 'translate(' + d.x + ',' + d.y + ')'; });
-    });
+    root = json;
+    update();
 });
